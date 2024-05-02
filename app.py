0847fe4a6e62faf4c88754f70fab0b7ad9ecf52c
Peakperformer <peakperformer@MacBook-Pro-33.local>
2024-05-02 18:43:13 +0530

    changes

--- app.py
+++ app.py
@@ -4,26 +4,37 @@ from io import BytesIO
 import base64
 from huggingface_hub import snapshot_download
 import os
+import requests
 
 class InferlessPythonModel:
     def initialize(self):
         print("Hello World 1")
-        self.pipe = StableDiffusionPipeline.from_pretrained(
-            "stabilityai/stable-diffusion-2-1",
-            use_safetensors=True,
-            torch_dtype=torch.float16,
-            device_map='auto'
-        )
 
 
     def infer(self, inputs):
         prompt = inputs["prompt"]
         print("Hello World Promt 15")
-        image = self.pipe(prompt).images[0]
-        buff = BytesIO()
-        image.save(buff, format="JPEG")
-        img_str = base64.b64encode(buff.getvalue()).decode()
+        url = "https://drive.google.com/file/d/1u7Ijpq9p8bNXHEs-5TK2Y9WsC-UONe1N/view?usp=sharing" 
+        img_str = url_to_base64 
         return { "generated_image_base64" : img_str }
         
     def finalize(self):
         self.pipe = None
+
+
+    def url_to_base64(self, image_url):
+        # Send a GET request to the image URL
+        response = requests.get(image_url)
+        # Raise an exception if the request was unsuccessful
+        response.raise_for_status()
+        
+        # Load the image content into a BytesIO stream
+        image_bytes = BytesIO(response.content)
+        
+        # Encode the image content in Base64
+        base64_encoded = base64.b64encode(image_bytes.getvalue())
+        
+        # Decode the Base64 bytes to a string
+        base64_string = base64_encoded.decode('utf-8')
+        
+        return base64_string
